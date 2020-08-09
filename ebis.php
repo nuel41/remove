2e8fad4bbc91b043503e245596183a0d1c96ef5a
saberwell04 <61140562+saberwell04@users.noreply.github.com>
2020-08-09 19:20:48 +0700

    New
    haha

---
+++ ebis.php
@@ -0,0 +1,22 @@
+<div id="container-user-akses">
+
+    <div class="name-bis">
+		<div class="element-form">
+			<label>Yance Immanuel (1318021)</label>			
+        </div>
+        
+		<div class="element-form">
+			<label>Ihsan Nurdwiansyah (1318011)</label>			
+        </div>        
+		<div class="element-form">
+			<label>Alby Alviawan Ramadhan (1318052)</label>			
+        </div>
+        
+		<div class="element-form">
+			<label>Retno Fatma  Larasati(1318065)</label>			
+        </div>        
+		<div class="element-form">
+			<label>Teresia Dewi Damanik (1318008)</label>			
+        </div>        
+	</div>
+</div>
